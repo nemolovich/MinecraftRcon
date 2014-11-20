38581d47e9009ebb27e43fb72095cd91c200ee4b
nemolovich <nemolovich@hotmail.fr>
2014-11-20 14:01:29 +0100

    CheckForUpdates need to be fixed

--- src/main/java/fr/nemolovich/apps/minecraftrcon/Launcher.java
+++ src/main/java/fr/nemolovich/apps/minecraftrcon/Launcher.java
@@ -3,6 +3,7 @@ package fr.nemolovich.apps.minecraftrcon;
 import fr.nemolovich.apps.minecraftrcon.exceptions.AuthenticationException;
 import fr.nemolovich.apps.minecraftrcon.exceptions.ConnectionException;
 import fr.nemolovich.apps.minecraftrcon.gui.MainFrame;
+
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Font;
@@ -18,15 +19,18 @@ import java.io.ObjectOutputStream;
 import java.net.JarURLConnection;
 import java.net.URL;
 import java.util.jar.Attributes;
+import java.util.jar.Manifest;
 import java.util.logging.Level;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
+
 import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
 import org.jdesktop.swingx.JXErrorPane;
@@ -34,170 +38,191 @@ import org.jdesktop.swingx.error.ErrorInfo;
 
 public class Launcher {
 
-    private static final Pattern HOST_PATTERN = Pattern.compile("^(?<host>("
-        + "(\\d{1,3}\\.){3}\\d{1,3})|([A-Za-z0-9.-]+))"
-        + "\\:(?<port>\\d+)$");
+	private static final Pattern HOST_PATTERN = Pattern.compile("^(?<host>("
+			+ "(\\d{1,3}\\.){3}\\d{1,3})|([A-Za-z0-9.-]+))"
+			+ "\\:(?<port>\\d+)$");
 
-    /**
-     * @param args
-     * @throws AuthenticationException
-     */
-    public static void main(String[] args) throws AuthenticationException {
-        BasicConfigurator.configure();
+	/**
+	 * @param args
+	 * @throws AuthenticationException
+	 */
+	public static void main(String[] args) throws AuthenticationException {
+		BasicConfigurator.configure();
 
-        args = new String[1];
-        args[0] = "-u";
-        if (args.length > 0) {
-            if (args[0].equalsIgnoreCase("--update")
-                || args[0].equalsIgnoreCase("-u")) {
-                checkForUpdates();
-                return;
-            }
-        }
+		args = new String[1];
+		args[0] = "-u";
+		if (args.length > 0) {
 
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager
-                .getInstalledLookAndFeels()) {
-                if ("Windows".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
+			int index = 0;
+			for (String arg : args) {
+				if (arg.equalsIgnoreCase("--update")
+						|| arg.equalsIgnoreCase("-u")) {
+					checkForUpdates();
+					return;
+				}
+				index++;
+			}
+		}
 
-                }
-            }
-        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(MainFrame.class.getName()).log(
-                java.util.logging.Level.SEVERE, null, ex);
-        }
-        JPanel panel = new JPanel(new BorderLayout(6, 6));
+		try {
+			for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager
+					.getInstalledLookAndFeels()) {
+				if ("Windows".equals(info.getName())) {
+					javax.swing.UIManager.setLookAndFeel(info.getClassName());
+					break;
 
-        JPanel header = new JPanel(new GridLayout(1, 0, 2, 2));
-        final String defaultText = "Please enter connection informations";
+				}
+			}
+		} catch (ClassNotFoundException | InstantiationException
+				| IllegalAccessException
+				| javax.swing.UnsupportedLookAndFeelException ex) {
+			java.util.logging.Logger.getLogger(MainFrame.class.getName()).log(
+					java.util.logging.Level.SEVERE, null, ex);
+		}
+		JPanel panel = new JPanel(new BorderLayout(6, 6));
 
-        final JLabel headerLabel = new JLabel(defaultText,
-            SwingConstants.CENTER);
+		JPanel header = new JPanel(new GridLayout(1, 0, 2, 2));
+		final String defaultText = "Please enter connection informations";
 
-        final Font defaultFont = headerLabel.getFont();
-        Font boldFont = new Font(defaultFont.getName(), Font.BOLD,
-            defaultFont.getSize());
-        final Color defaultColor = headerLabel.getForeground();
-        header.add(headerLabel);
-        panel.add(header, BorderLayout.NORTH);
+		final JLabel headerLabel = new JLabel(defaultText,
+				SwingConstants.CENTER);
 
-        JPanel label = new JPanel(new GridLayout(0, 1, 2, 2));
-        label.add(new JLabel("Host", SwingConstants.RIGHT));
-        label.add(new JLabel("Password", SwingConstants.RIGHT));
-        panel.add(label, BorderLayout.WEST);
+		final Font defaultFont = headerLabel.getFont();
+		Font boldFont = new Font(defaultFont.getName(), Font.BOLD,
+				defaultFont.getSize());
+		final Color defaultColor = headerLabel.getForeground();
+		header.add(headerLabel);
+		panel.add(header, BorderLayout.NORTH);
 
-        JPanel controls = new JPanel(new GridLayout(0, 1, 2, 2));
-        JTextField hostField = new JTextField("localhost:25575");
-        controls.add(hostField);
-        hostField.addKeyListener(new KeyAdapter() {
+		JPanel label = new JPanel(new GridLayout(0, 1, 2, 2));
+		label.add(new JLabel("Host", SwingConstants.RIGHT));
+		label.add(new JLabel("Password", SwingConstants.RIGHT));
+		panel.add(label, BorderLayout.WEST);
 
-            @Override
-            public void keyTyped(KeyEvent e) {
-                super.keyTyped(e);
-                headerLabel.setText(defaultText);
-                headerLabel.setFont(defaultFont);
-                headerLabel.setForeground(defaultColor);
-            }
-        });
-        JPasswordField passwordField = new JPasswordField();
-        controls.add(passwordField);
-        panel.add(controls, BorderLayout.CENTER);
+		JPanel controls = new JPanel(new GridLayout(0, 1, 2, 2));
+		JTextField hostField = new JTextField("localhost:25575");
+		controls.add(hostField);
+		hostField.addKeyListener(new KeyAdapter() {
 
-        hostField.setText("raspberry:20066");
-        passwordField.setText("Minecraft2580");
+			@Override
+			public void keyTyped(KeyEvent e) {
+				super.keyTyped(e);
+				headerLabel.setText(defaultText);
+				headerLabel.setFont(defaultFont);
+				headerLabel.setForeground(defaultColor);
+			}
+		});
+		JPasswordField passwordField = new JPasswordField();
+		controls.add(passwordField);
+		panel.add(controls, BorderLayout.CENTER);
 
-        String host;
+		hostField.setText("raspberry:20066");
+		passwordField.setText("Minecraft2580");
 
-        Matcher matcher = null;
+		String host;
 
-        while (matcher == null || !matcher.matches()) {
-            if (JOptionPane.showConfirmDialog(null, panel, "Host informations",
-                JOptionPane.OK_CANCEL_OPTION) == JOptionPane.OK_OPTION) {
-                host = hostField.getText();
-                matcher = HOST_PATTERN.matcher(host);
-                if (!matcher.matches()) {
-                    headerLabel.setForeground(Color.RED);
-                    headerLabel.setFont(boldFont);
-                    headerLabel.setText("Host format: <HOST>:<PORT>");
-                }
-            } else {
-                return;
-            }
-        }
+		Matcher matcher = null;
 
-        final String hostName = matcher.group("host");
-        final int hostPort = Integer.parseInt(matcher.group("port"));
-        final String password = new String(passwordField.getPassword());
+		while (matcher == null || !matcher.matches()) {
+			if (JOptionPane.showConfirmDialog(null, panel, "Host informations",
+					JOptionPane.OK_CANCEL_OPTION) == JOptionPane.OK_OPTION) {
+				host = hostField.getText();
+				matcher = HOST_PATTERN.matcher(host);
+				if (!matcher.matches()) {
+					headerLabel.setForeground(Color.RED);
+					headerLabel.setFont(boldFont);
+					headerLabel.setText("Host format: <HOST>:<PORT>");
+				}
+			} else {
+				return;
+			}
+		}
 
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                MainFrame mf = null;
-                try {
-                    mf = new MainFrame(new ClientSocket(
-                        hostName, hostPort, password));
-                    mf.setVisible(true);
-                } catch (AuthenticationException | ConnectionException e) {
-                    if (mf != null) {
-                        mf.close();
-                    }
-                    JXErrorPane.showDialog(mf,
-                        new ErrorInfo("Connection error",
-                            String.format("The connection to host '%s' failed",
-                                hostName),
-                            null, "Error", e, Level.SEVERE, null));
-                }
-            }
-        });
-    }
+		final String hostName = matcher.group("host");
+		final int hostPort = Integer.parseInt(matcher.group("port"));
+		final String password = new String(passwordField.getPassword());
 
-    public static final void doNotUse(String chose, String machin) throws IOException {
+		/* Create and display the form */
+		java.awt.EventQueue.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				MainFrame mf = null;
+				try {
+					mf = new MainFrame(new ClientSocket(hostName, hostPort,
+							password));
+					mf.setVisible(true);
+				} catch (AuthenticationException | ConnectionException e) {
+					if (mf != null) {
+						mf.close();
+					}
+					JXErrorPane.showDialog(
+							mf,
+							new ErrorInfo("Connection error", String.format(
+									"The connection to host '%s' failed",
+									hostName), null, "Error", e, Level.SEVERE,
+									null));
+				}
+			}
+		});
+	}
 
-        File f = new File(chose);
+	public static final void doNotUse(String chose, String machin)
+			throws IOException {
 
-        URL truc = new URL(machin);
+		File f = new File(chose);
 
-        Attributes bidule = getManifestAttributes(truc);
-        System.out.println(bidule.getValue(Attributes.Name.IMPLEMENTATION_VERSION));
+		URL truc = new URL(machin);
 
-        try (ObjectOutputStream porte = new ObjectOutputStream(
-            new BufferedOutputStream(new FileOutputStream(f)))) {
-            porte.writeChars(bidule.getValue(Attributes.Name.IMPLEMENTATION_VERSION));
-        }
-    }
+		JarURLConnection chouette = (JarURLConnection) truc.openConnection();
 
-    private static Attributes getManifestAttributes(URL url) throws IOException {
+		Attributes bidule = chouette.getMainAttributes();
+		System.out.println(bidule
+				.getValue(Attributes.Name.IMPLEMENTATION_VERSION));
 
-        JarURLConnection conn = (JarURLConnection) url.openConnection();
+		try (ObjectOutputStream porte = new ObjectOutputStream(
+				new BufferedOutputStream(new FileOutputStream(f)))) {
+			porte.writeChars(bidule
+					.getValue(Attributes.Name.IMPLEMENTATION_VERSION));
+		}
+	}
 
-        return conn.getMainAttributes();
-    }
+	private static void checkForUpdates() {
+		try {
+			Logger.getLogger(Launcher.class).info("Check for updates");
+			final URL localURL = Launcher.class
+					.getResource("/META-INF/MANIFEST.MF");
 
-    private static void checkForUpdates() {
-        try {
-            final URL url = new URL("http://cdn.rawgit.com/nemolovich/MinecraftRcon/master/MinecraftRcon.db");
+			Manifest manifest = new Manifest(localURL.openStream());
 
-            StringBuilder version;
-            try (ObjectInputStream is = new ObjectInputStream(
-                url.openStream())) {
-                version = new StringBuilder();
-                while (is.available() > 0) {
-                    version.append(is.readChar());
-                }
-            }
-            double remoteVersion;
-            try {
-                remoteVersion = Double.valueOf(version.toString());
-            } catch (NumberFormatException e) {
-                throw new IOException("Unavailable remote version");
-            }
-            Logger.getLogger(Launcher.class).info("Remote version: " + remoteVersion);
+			Attributes attrs = manifest.getMainAttributes();
 
-        } catch (IOException ex) {
-            Logger.getLogger(Launcher.class).error("Can not retrieve remote version", ex);
-        }
-    }
+			double currentVersion = Double.valueOf(attrs
+					.getValue(Attributes.Name.IMPLEMENTATION_VERSION));
+			Logger.getLogger(Launcher.class).info(
+					"Current version: " + currentVersion);
+
+			final URL url = new URL(
+					"http://cdn.rawgit.com/nemolovich/MinecraftRcon/master/MinecraftRcon.db");
+
+			StringBuilder version;
+			try (ObjectInputStream is = new ObjectInputStream(url.openStream())) {
+				version = new StringBuilder();
+				while (is.available() > 0) {
+					version.append(is.readChar());
+				}
+			}
+			double remoteVersion;
+			try {
+				remoteVersion = Double.valueOf(version.toString());
+			} catch (NumberFormatException e) {
+				throw new IOException("Unavailable remote version");
+			}
+			Logger.getLogger(Launcher.class).info(
+					"Remote version: " + remoteVersion);
+
+		} catch (IOException ex) {
+			Logger.getLogger(Launcher.class).error(
+					"Can not retrieve remote version", ex);
+		}
+	}
 }
